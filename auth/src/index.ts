bbabddab394629c6967c8ed27bd4ac8ec95d3f35
Etienne Estrangin <etienne.estrangin@gmail.com>
2020-07-28 11:03:41 +0200

    section 5 - 102: ingress-nginx setup for auth service

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,10 @@ import { json } from 'body-parser';
 const app = express();
 app.use(json());
 
+app.get('/api/users/currentuser', (req, res) => {
+  res.send('/api/users/currentuser route has been called with get HTTP method');
+});
+
 app.listen(3000, () => {
   console.log('auth service is listening on port 3000');
 });
