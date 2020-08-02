23e601713f4b4e16fdaf4176cfa239b0ff98ec69
Etienne Estrangin <etienne.estrangin@gmail.com>
2020-08-02 18:27:27 +0200

    section 8 - 137: mongoose initialization and UserModel creation

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,12 +1,15 @@
 import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
+import mongoose from 'mongoose';
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
 import { errorHandler } from './middlewares/error-handler';
 import { NotFoundError } from './errors/not-found-error';
+
+import { User } from './model/user';
 
 const app = express();
 app.use(json());
@@ -22,6 +25,21 @@ app.all('*', () => {
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log('auth service is listening on port 3000');
-});
+const start = async () => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log('connected to mongo db');
+  } catch (err) {
+    console.log(err);
+  }
+
+  app.listen(3000, () => {
+    console.log('auth service is listening on port 3000');
+  });
+};
+
+start();
