b2e18dd33f1d2b46a52903585ae367d0fca9ca68
Etienne Estrangin <etienne.estrangin@gmail.com>
2020-08-04 23:08:53 +0200

    section 9 - 162: authentication setup

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # ticketing-microservices
 
 This application is ticketing web application based on microservices architecture implemented during a Udemy online course [Microservices with nodeJS and React](https://www.udemy.com/course/microservices-with-node-js-and-react/).
+
+## Install
+
+Initialize JWT Token secret key
+
+```
+kubectl create secret generic jwt-secret --from-literal=JWT_KEY=SECRET_PRIVATE_KEY
+```
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import jwt from 'jsonwebtoken';
 import { BadRequestError } from '../errors/bad-request-error';
 import { RequestValidationError } from '../errors/request-validation-error';
 import { User } from '../model/user';
@@ -31,6 +32,21 @@ router.post(
 
     const user = User.build({ email, password });
     await user.save();
+
+    // Generate the JWT
+    const userJwt = jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+      },
+      process.env.JWT_KEY!
+    );
+
+    // Store it to the session object
+    req.session = {
+      jwt: userJwt,
+    };
+
     res.status(201).send(user);
   }
 );
