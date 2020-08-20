f2578b1ec3be3d93b77bec5dcb85e40fda7cec08
Etienne Estrangin <etienne.estrangin@gmail.com>
2020-08-20 12:50:28 +0200

    section 12 - 232 - common module, typescript integration

---
+++ common/src/index.ts
@@ -0,0 +1,9 @@
+import Color from './model/color';
+
+const color: Color = {
+  red: 10,
+  green: 10,
+  blue: 10,
+};
+
+export default color;
---
+++ common/src/model/color.ts
@@ -0,0 +1,5 @@
+export default interface Color {
+  red: number;
+  blue: number;
+  green: number;
+}
