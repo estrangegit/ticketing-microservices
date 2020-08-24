ba1ca7171aa0e6b2f24ca6aa7b2266a823eeb1a7
Etienne Estrangin <etienne.estrangin@gmail.com>
2020-08-24 19:55:34 +0200

    section 13 - 239 - ticket service initialization

---
+++ tickets/src/index.ts
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+import { app } from './app';
+
+const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT_KEY must be defined');
+  }
+
+  try {
+    await mongoose.connect('mongodb://tickets-mongo-srv:27017/tickets', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log('connected to mongo db');
+  } catch (err) {
+    console.log(err);
+  }
+
+  app.listen(3000, () => {
+    console.log('auth service is listening on port 3000');
+  });
+};
+
+start();
