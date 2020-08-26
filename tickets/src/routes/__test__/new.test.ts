18f1bbf8dbcf24aea0baae3304c645cc236e440b
Etienne Estrangin <etienne.estrangin@gmail.com>
2020-08-26 17:08:02 +0200

    section 13 - 247 - auth and body validation implementation

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -23,8 +23,60 @@ it('returns a status other than 401 if the user is signed in', async () => {
   expect(response.status).not.toEqual(401);
 });
 
-it('returns an error if an invalid title is provided', async () => {});
+it('returns an error if an empty title is provided', async () => {
+  const cookie = await signinHelper('test@test.com');
 
-it('returns an error if an invalid price is provided', async () => {});
+  const response = await request(app)
+    .post('/api/tickets')
+    .set('Cookie', cookie)
+    .send({
+      title: '',
+      price: '10',
+    });
 
-it('creates a ticket with valid inputs', async () => {});
+  expect(response.status).toEqual(400);
+});
+
+it('returns an error if no title is provided', async () => {
+  const cookie = await signinHelper('test@test.com');
+
+  const response = await request(app)
+    .post('/api/tickets')
+    .set('Cookie', cookie)
+    .send({
+      price: 10,
+    });
+
+  expect(response.status).toEqual(400);
+});
+
+it('returns an error if the provided price is negative', async () => {
+  const cookie = await signinHelper('test@test.com');
+
+  const response = await request(app)
+    .post('/api/tickets')
+    .set('Cookie', cookie)
+    .send({
+      title: 'title',
+      price: -10,
+    });
+
+  expect(response.status).toEqual(400);
+});
+
+it('creates a ticket with valid inputs', async () => {
+  const cookie = await signinHelper('test@test.com');
+
+  const title: string = 'title';
+  const price: number = 10;
+
+  const response = await request(app)
+    .post('/api/tickets')
+    .set('Cookie', cookie)
+    .send({
+      title: title,
+      price: price,
+    });
+
+  expect(response.status).toEqual(201);
+});
--- tickets/src/routes/news.ts
+++ tickets/src/routes/news.ts
@@ -1,13 +1,24 @@
-import { requireAuth } from '@ettickets/common';
+import { requireAuth, validateRequest } from '@ettickets/common';
 import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
 
 const router = express.Router();
 
 router.post(
   '/api/tickets',
+  [
+    body('title')
+      .trim()
+      .notEmpty()
+      .withMessage('You must supply a non empty title'),
+    body('price')
+      .isFloat({ gt: 0 })
+      .withMessage('You must supply a price greater than 0'),
+  ],
   requireAuth,
+  validateRequest,
   async (req: Request, res: Response) => {
-    res.sendStatus(200);
+    res.sendStatus(201);
   }
 );
 
