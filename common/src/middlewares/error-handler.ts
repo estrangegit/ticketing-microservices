896184732ccbb43e0a34c8ac9b7835b1df6a5ced
Etienne Estrangin <etienne.estrangin@gmail.com>
2020-09-02 12:10:39 +0200

    section 13 - 254 - better error logging

--- common/src/middlewares/error-handler.ts
+++ common/src/middlewares/error-handler.ts
@@ -11,6 +11,8 @@ export const errorHandler = (
     return res.status(err.statusCode).send({ errors: err.serializeErrors() });
   }
 
+  console.error(err);
+
   res.status(500).send({
     errors: [{ message: 'Something went wrong' }],
   });
