e03f2f4c5a2216a001e4ff7bd5f8af98033ed62e
Etienne Estrangin <etienne.estrangin@gmail.com>
2020-09-17 15:02:28 +0200

    section 17 - 330 - find reserved tickets

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,7 +1,15 @@
-import { requireAuth, validateRequest } from '@ettickets/common';
+import {
+  BadRequestError,
+  NotFoundError,
+  OrderStatus,
+  requireAuth,
+  validateRequest,
+} from '@ettickets/common';
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import mongoose from 'mongoose';
+import { Order } from '../models/order';
+import { Ticket } from '../models/ticket';
 
 const router = express.Router();
 
@@ -16,6 +24,39 @@ router.post(
   ],
   validateRequest,
   async (req: Request, res: Response) => {
+    const { ticketId } = req.body;
+
+    const existingTicket = await Ticket.findById(ticketId);
+
+    if (!existingTicket) {
+      throw new NotFoundError();
+    }
+
+    // Find the ticket the user is trying to order in the database
+    const existingOrder = await Order.findOne({
+      ticket: existingTicket,
+      status: {
+        $in: [
+          OrderStatus.AwaitingPayment,
+          OrderStatus.Created,
+          OrderStatus.Complete,
+        ],
+      },
+    })
+      .populate({ path: 'ticket', _id: ticketId })
+      .exec();
+
+    if (existingOrder) {
+      throw new BadRequestError('ticket is already reserved');
+    }
+    // Make sure that this ticket is not already reserved
+
+    // Calcluate an expiration date for this order
+
+    // Build the order and save it to the database
+
+    // Publish an event saying that an order was created
+
     res.send({});
   }
 );
