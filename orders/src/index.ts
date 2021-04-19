4587b756efec5908274c55c655f18c25b9e4d22c
Etienne Estrangin <etienne.estrangin@gmail.com>
2021-04-19 23:32:40 +0200

    section 23 - 475 verifying a test run

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -8,6 +8,7 @@ import { natsWrapper } from './nats-wrapper';
 
 const start = async () => {
   console.log('orders service is starting up');
+
   if (!process.env.MONGO_URI) {
     throw new Error('MONGO_URI must be defined');
   }
